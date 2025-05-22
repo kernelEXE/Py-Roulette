c735138942cb586efb631733046a366f27bbfc91
Adair Junior <adaircydia@gmail.com>
2025-05-22 18:12:40 -0300

    Update roleta.py v2
    Foram feitas melhorias

--- roleta.py
+++ roleta.py
@@ -1,6 +1,5 @@
 #roleta russa 
 ########################
-import random
 import time
 from time import sleep
 import os
@@ -8,25 +7,25 @@ import random
 from pygame import mixer
 ########################
 #audios do jogo 
+mixer.init()
 def audio0():
-    mixer.init()
     mixer.music.load("Py-Roulette/suspense.mp3")
     mixer.music.play(-1)
 def audio1():
-    mixer.init()
     mixer.music.load("Py-Roulette/roleta.mp3")
     mixer.music.play(2)
 def audio2():
-    mixer.init()
     mixer.music.load("Py-Roulette/shot.mp3")
     mixer.music.play()
 def audio3():
-    mixer.init()
     mixer.music.load("Py-Roulette/cock.mp3")
     mixer.music.play()
 def audio4():
     mixer.init()
     mixer.music.load("Py-Roulette/happy.mp3")
+    mixer.music.play(-4)
+def audio5():
+    mixer.music.load("Py-Roulette/soviet.mp3")
     mixer.music.play(-4)
 ########################
 #atalhos
@@ -38,7 +37,6 @@ def rand0n():
     return random.randint(1,6)
 ########################
 #Codigos do Game
-
 def jogo():
     limpa()
     audio0()
@@ -47,12 +45,19 @@ def jogo():
     p("O jogo consiste em uma roleta russa,voce escolhe um numero,")
     p("e se ele for sorteando você vai de vasco")
     p("\n")
-    num = int(input("Escolha um numero ( 1 / 6 ) : "))
-    if num <1 or num > 6:
-        limpa()
-        p("Numero invalido tente novamente")
-        sleep(2)
-        jogo()
+    while True:
+        try:
+            num = int(input("Escolha um numero ( de 1 a 6 ) : "))
+            if 1 <= num <= 6:
+                break
+            else:
+                limpa()
+                print("❌ Por favor, escolha um número entre 1 e 6.")
+                sleep(2)
+                limpa()
+        except ValueError:
+            limpa()
+            print("Digite apenas números!")
     sleep(2)
     limpa()
     audio1()
@@ -82,34 +87,39 @@ def jogo():
     p(f"Numero escolhido : 💀 {num} 💀")
     if num == disparo:
         audio2()
+        
         p(f"💥 SE FUDEU OTÁRIO 💀, o disparo deu {disparo}")
+        sleep(0.7)
+        audio5()
         sleep(5)
         limpa()
-        repetir()
+        
     else:
         p(f"😅 Você se safou! O tambor parou no {disparo}")
         audio4()
         sleep(6)
         limpa()
-        repetir()
+        
 def repetir():
     while True:
-        resposta = input("Deseja jogar novamente? (S/N): ").lower()
+        jogo()
+        while True: 
+            resposta = input("Deseja jogar novamente? (S/N): ").lower()
 
-        if resposta == "s":
-            jogo()
-            return True
-        elif resposta == "n":
-            limpa()
-            p("Obrigado por jogar, vejo você em breve.")
-            sleep(2)
-            return False
-        else:
-            limpa()
-            p("Você deve digitar apenas S ou N.")
-            sleep(2)
-            limpa()
+            if resposta == "s":
+                limpa()
+                break
+            elif resposta == "n":
+                limpa()
+                p("Obrigado por jogar, vejo você em breve.")
+                sleep(2)
+                limpa()
+                return
+            else:
+                limpa()
+                p("Você deve digitar apenas S ou N.")
+                sleep(2)
+                limpa()
+            
 
-
-
-jogo()
+repetir()
